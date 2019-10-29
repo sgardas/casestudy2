ce84ca26fcf9e3d1eb2446709426ce003c335cf3
keerthi <keerthisagarbukkapuram@gmail.com>
2019-10-29 14:32:33 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
